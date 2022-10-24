cc41323f31b5003d229b230b104d59795589ed14
SONSAZANG <kekekee257@gmail.com>
2022-10-25 00:33:28 +0900

    [fastcampus_Unity]_081_class_parameter

---
+++ fastcampus_Unity/_078_Constructor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _078_Constructor
+{
+    class AA
+    {
+        int a;
+        float f;
+
+        // 생성자는 클래스 이름과 동일하다.
+        public AA()
+        {
+            a = 0;
+            f = 0f;
+            Console.WriteLine(" Call Constructor ");
+            Console.WriteLine(" a: {0}", a);
+            Console.WriteLine(" f: {0}", f);
+        }
+
+        public AA(int _a, float _f)
+        {
+            a = _a;
+            f = _f;
+
+            Console.WriteLine(" Call Constructor ");
+            Console.WriteLine(" a: {0}", a);
+            Console.WriteLine(" f: {0}", f);
+        }
+
+        // 소멸자는 매개변수 선언이 불가능하다.
+        ~AA()
+        {
+            Console.WriteLine(" Call Destructor ");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            AA ab = new AA(15, 15.4f);
+        }
+    }
+}
---
+++ fastcampus_Unity/_080_static/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _080_static
+{
+    class AA
+    {
+        public static int a;
+        public static int b;
+        public static readonly int c = 200; // 상수화된다.
+
+        public static void Print()
+        {
+            Console.WriteLine("a: {0}", a);
+            Console.WriteLine("b: {0}", b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA.a = 10;
+            AA.b = 100;
+            // AA.c = 200; 오류
+
+            AA.Print();
+        }
+    }
+}
